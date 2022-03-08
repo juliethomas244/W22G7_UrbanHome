a1e6e09a6a18bc714ad789377f0c5ddcc3271626
ChelsyChelsy <chelsydhir@gmail.com>
2022-03-07 22:23:14 -0800

    Update HomeActivity.java
    grid view categories

--- app/src/main/java/com/example/urbanhomeapp/HomeActivity.java
+++ app/src/main/java/com/example/urbanhomeapp/HomeActivity.java
@@ -2,8 +2,14 @@ package com.example.urbanhomeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Selection;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,8 +33,25 @@ public class HomeActivity extends AppCompatActivity {
         HomeAdapter homeAdapter = new HomeAdapter(imgNames, imgPics);
         gridViewCategory.setAdapter(homeAdapter);
         gridViewCategory.setNumColumns(3);
-        gridViewCategory.setVerticalSpacing(8);
-        gridViewCategory.setHorizontalSpacing(8);
+        gridViewCategory.setVerticalSpacing(5);
+        gridViewCategory.setHorizontalSpacing(5);
         gridViewCategory.setScrollBarSize(3);
+
+        //When the user clicks any item from the category, the app will take the user to the next
+        // activity showing the GridView of corresponding items
+        gridViewCategory.setOnItemClickListener((AdapterView<?> adapterView, View view, int position, long l) -> {
+            //position is used here to access the item in the gridView
+
+            if(position == 0)//When chair is clicked show all the types of chairs in another activity
+                startActivity(new Intent(HomeActivity.this, ChairsActivity.class));
+
+            if(position == 1)//When bed is clicked show all the types of bed in another activity
+                startActivity(new Intent(HomeActivity.this, BedActivity.class));
+
+            if(position == 2)//When table is clicked show all the types of tables in another activity
+                startActivity(new Intent(HomeActivity.this, TablesActivity.class));
+        });
+
+
     }
 }
