ccbfa17fcae06f964b116d85c9f6f97c838db1e2
juliethomas244 <thomasj9@student.douglascollege.ca>
2022-03-08 16:50:58 -0800

    Checkout page
    User can check the items added to the cart, input delivery details and place order

---
+++ app/src/main/java/com/example/urbanhomeapp/CheckoutActivity.java
@@ -0,0 +1,55 @@
+package com.example.urbanhomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+        RadioGroup radioGroupDel = findViewById(R.id.radioGroup);
+        Button btnBuyNow = findViewById(R.id.buttonBuyNow);
+        //ListView listViewDelivery = findViewById(R.id.listViewDelivery);
+        LinearLayout hiddenDeliveryLayout = findViewById(R.id.hiddenCheckoutLayout);
+
+
+        radioGroupDel.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                int radiBtnID = radioGroup.getCheckedRadioButtonId();
+
+                if (radiBtnID == R.id.radioButtonPickup){
+                    Toast.makeText(CheckoutActivity.this,
+                            "Pickup Delivery selected",
+                            Toast.LENGTH_SHORT).show();
+                    hiddenDeliveryLayout.setVisibility(View.GONE);
+                } else if (radiBtnID == R.id.radioButtonDelivery) {
+                    Toast.makeText(CheckoutActivity.this,
+                            "Home Delivery selected",
+                            Toast.LENGTH_SHORT).show();
+                    hiddenDeliveryLayout.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+        });
+
+
+
+        btnBuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CheckoutActivity.this, "Order placed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
