683efab09c7b3aa7227274bac49ad3d55b302126
ChelsyChelsy <chelsydhir@gmail.com>
2022-03-09 13:05:31 -0800

    Update TablesActivity.java
    Modifications

--- app/src/main/java/com/example/urbanhomeapp/TablesActivity.java
+++ app/src/main/java/com/example/urbanhomeapp/TablesActivity.java
@@ -13,23 +13,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TablesActivity extends AppCompatActivity {
-    Toast toast = new Toast(this);
     GridView gridviewTables;
-    List<String> tableNames = new ArrayList<>(Arrays.asList("House of Hampton Caputo 47.5 Console Table"
-            , "VASAGLE Round End Table with Storage Shelf",
-            "ABS Plastic Folding Table - 72 x 30 x 29, Beige", "Griffin Reclaimed Wood Round Coffee Table",
-            "Clearwater 37.75'' Console Table", "Alexious 47'' Console Table By Everly Quinn"));
-
-    List<Integer> tablePics = new ArrayList<>(Arrays.asList(R.drawable.hampton,
-            R.drawable.endtable, R.drawable.abs, R.drawable.griffin, R.drawable.laurel, R.drawable.everly));
+    List<String> tableNames;
+    List<Integer> tablePics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tables);
+
+        tableNames = new ArrayList<>(Arrays.asList("House of Hampton Caputo 47.5 Console Table"
+                , "VASAGLE Round End Table with Storage Shelf",
+                "ABS Plastic Folding Table - 72 x 30 x 29, Beige", "Griffin Reclaimed Wood Round Coffee Table",
+                "Clearwater 37.75'' Console Table", "Alexious 47'' Console Table By Everly Quinn"));
+
+        tablePics = new ArrayList<>(Arrays.asList(R.drawable.hampton,
+                R.drawable.endtable, R.drawable.abs, R.drawable.griffin,
+                R.drawable.laurel, R.drawable.everly));
+
         gridviewTables = findViewById(R.id.gridviewTables);
-        TableAdapter tableAdapter = new TableAdapter(tableNames, tablePics);
+        CategoriesAdapter tableAdapter = new CategoriesAdapter(tableNames, tablePics);
         gridviewTables.setAdapter(tableAdapter);
+
         gridviewTables.setHorizontalSpacing(8);
         gridviewTables.setVerticalSpacing(8);
         gridviewTables.setScrollBarSize(3);
@@ -41,7 +46,7 @@ public class TablesActivity extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                Toast.makeText(TablesActivity.this, "You Clicked "+ tableNames.get(i), Toast.LENGTH_SHORT).show();
             }
         });
     }
