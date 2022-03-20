73af211163a2f352a4fc817ab18d76d8ebfb9d58
ChelsyChelsy <chelsydhir@gmail.com>
2022-03-19 17:32:39 -0700

    Update HomeActivity.java
    changes

--- app/src/main/java/com/example/urbanhomeapp/HomeActivity.java
+++ app/src/main/java/com/example/urbanhomeapp/HomeActivity.java
@@ -3,11 +3,14 @@ package com.example.urbanhomeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Selection;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -17,6 +20,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    EditText editTextSearch;
     GridView gridViewCategory;
     ImageView imgViewCart;
     List<String> imgNames = new ArrayList<>(
@@ -39,6 +43,10 @@ public class HomeActivity extends AppCompatActivity {
         gridViewCategory.setVerticalSpacing(5);
         gridViewCategory.setHorizontalSpacing(5);
         gridViewCategory.setScrollBarSize(3);
+
+        editTextSearch = findViewById(R.id.editTextSearch);
+        String search = editTextSearch.getText().toString();
+
 
         //When the user clicks any item from the category, the app will take the user to the next
         // activity showing the GridView of corresponding items
