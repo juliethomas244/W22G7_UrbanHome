0cf156f5a79c17d7346d6d8cbc5233d761965f5b
juliethomas244 <thomasj9@student.douglascollege.ca>
2022-03-20 19:02:35 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/juliethomas244/W22G7_UrbanHome""
    This reverts commit c169ebf751c50b7694ac33caabc0c6c6eda85899.

---
+++ app/src/main/java/com/example/urbanhomeapp/MenuActivity.java
@@ -0,0 +1,48 @@
+package com.example.urbanhomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MenuActivity extends AppCompatActivity {
+    List<String> menu = new ArrayList<>(
+            Arrays.asList("Sign In", "Sign Out", "Cart", "Policies", "Feedback", "Customer Support")
+    );
+    ListView listViewMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        listViewMenu = findViewById(R.id.listViewMenu);
+        MenuAdapter menuAdapter = new MenuAdapter(menu);
+        listViewMenu.setAdapter(menuAdapter);
+
+        listViewMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    //("Sign In", "Sign Out", "Cart", "Policies", "Feedback", "Customer Support")
+                    case 3:
+                        startActivity(new Intent(MenuActivity.this, PoliciesActivity.class));
+                        break;
+                    case 4:
+                        startActivity(new Intent(MenuActivity.this, FeedbackActivity.class));
+                        break;
+                    case 5:
+                        startActivity(new Intent(MenuActivity.this, CustomerSupportActivity.class));
+                        break;
+                }
+            }
+        });
+    }
+}
